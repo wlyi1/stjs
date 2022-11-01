00f4f922d212395a498eba7d991f457334da5a0f
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-11-01 10:40:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,24 @@ st.title('Polling Kehidupan')
 st.caption("polling kehidupan merupakan platform untuk ngasih tau ke masyarakat tentang fenomena kehidupan apa adanya")
 st.subheader("Apakah pasanganmu saat ini adalah yang terbaik?")
 col1, col2 = st.columns(2)
-ya = 0
-tidak = 0
+
+if 'ya' not in session_state:
+    st.session_state.ya = 0
+    
+if 'tidak' not in session_state:
+    st.session_state.tidak = 0
+
 with col1:
     ya_but = st.button('Ya')
     st.text_area('Kenapa bisa iya')
     if ya_but:
-        ya +=1
+        st.session_state.ya +=1
 with col2:
     tidak_but = st.button('Tidak')
     st.text_area('Kenapa bisa tidak')
     if tidak_but:
-        tidak += 1
+        st.session_state.tidak += 1
        
     
-st.write(ya)
-st.write(tidak)
+st.write(st.session_state.ya)
+st.write(st.session_state.tidak)

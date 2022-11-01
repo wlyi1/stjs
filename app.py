3fc501560ee5d714c39f9d8d5210021930a0be9f
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-11-01 10:32:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,23 @@ from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share
 import json
 import time
 
-if st.checkbox("Check my location"):
-    loc = get_geolocation()
-    with st.spinner('waiting'):
-        time.sleep(3)
-    st.write(f"Your coordinates are {loc}")
-    st.write(loc['coords']['latitude'])
-    st.write(loc['coords']['longitude'])
+st.title('Polling Kehidupan')
+st.caption("polling kehidupan merupakan platform untuk ngasih tau ke masyarakat tentang fenomena kehidupan apa adanya")
+st.subheader("Apakah pasanganmu saat ini adalah yang terbaik?")
+col1, col2, st.column(2)
+ya = 0
+tidak = 0
+with col1:
+    ya_but = st.button('Ya')
+    st.text_area('Kenapa bisa iya')
+    if ya_but:
+        ya +=1
+with col2:
+    tidak_but = st.button('Tidak')
+    st.text_area('Kenapa bisa tidak')
+    if tidak_but:
+        tidak += 1
+       
+    
+st.write(ya)
+st.write(tidak)

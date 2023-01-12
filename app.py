e6edafa7c700d984ec37d1ac73c91485805380c5
waliyudin sammadikun <waliyudin938@gmail.com>
2023-01-12 15:21:02 +0700

    ss

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="katanya-85289")
 
-hari = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+hari = dt.now() #.strftime('%Y-%m-%d %H:%M:%S')
 
 st.title('Katanya?!')
 st.write("Ehh tau ga sih katanya bla bla bla ahh coba lo buktiin di mari dah! 😮‍💨 ")

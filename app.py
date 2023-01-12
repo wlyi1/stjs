878228b4bf1b646f0a23708bd8f2dcb432da8ce3
waliyudin sammadikun <waliyudin938@gmail.com>
2023-01-12 13:20:17 +0700

    emoji

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ db = firestore.Client(credentials=creds, project="katanya-85289")
 
 
 st.title('Katanya?!')
-st.write("Ehh tau ga sih katanya ini? katanya itu? ahh coba lo buktiin di mari dah!")
+st.write("Ehh tau ga sih katanya ini? katanya itu? ahh coba lo buktiin di mari dah! 😮‍💨 ")
 
 with st.form(key='form1'):
+    st.subheader("Apakah pasanganmu saat ini adalah yang terbaik? 🫢")
     cols1, cols2 = st.columns(2)
-    st.subheader("Apakah pasanganmu saat ini adalah yang terbaik?")
     agree = cols1.checkbox('Iya')
     disagree = cols2.checkbox('Engga')
     text_input = st.text_area(label='Ceritanya gimana')

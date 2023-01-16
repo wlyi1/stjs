f9b060d30a9a2c71ef470ab5e2d174e8414e19a3
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2023-01-16 20:28:29 +0700

    add nikah section

--- katanya.py
+++ katanya.py
@@ -51,26 +51,25 @@ def pasangan():
     for i,j,k,l in zip(data['nama'], data['cerita'], data['option'], data['tanggal']):
         cerita_list = list(j.split(" "))
         if k == 'Iya':
-            st.info(f'**{i.capitalize()}: {" ".join(cerita_list[:7])}  ({l.strftime("%Y-%m-%d")})**')
+            st.info(f'**{i.capitalize()}: {" ".join(cerita_list[:7])}**')
         else:
-            st.success(f'**{i.capitalize()} : {" ".join(cerita_list[:7])}  -- ({l.strftime("%Y-%m-%d")})**')
+            st.error(f'**{i.capitalize()} : {" ".join(cerita_list[:7])}**')
         st.write(j)
 
 def nikah():
     hari = dt.now() #.strftime('%Y-%m-%d %H:%M:%S')
 
     st.header("Apakah pernikahanmu bahagia? 🥰")
-    st.caption("Memiliki pasangan bagi kebanyakan orang membuatnya menjadi lebih bahagia dan hidupnya lebih berwarna. \
-    Tapi ada yang bilang setelah punya pasangan malah membuat hidupnya makin merana dan tragis dibandingkan ketika masih single\
-        Jadi bagaimana denganmu? Bisakah kau ceritakan apa adanya sehingga orang lain dapat belajar dan memahami dari pengalaman kamu?")
+    st.caption("Katanya pernikahan membuat hidup kita menjadi lengkap dan indah. Ehm tapi ada yang bilang \
+    hidupnya hancur setelah menikah. Oke, jadi bagaimana denganmu apakah setelah menikah bahagia?")
 
     with st.form(key='form1', clear_on_submit=True):
         
-        col1 = db.collection('pasangan')
+        col1 = db.collection('nikah')
         name = st.text_input('Nama/akronim/samaran/panggilan : ')
-        st.write("Apakah pasanganmu adalah yang terbaik? 🥰")
+        st.write("Apakah pernikahanmu bahagia? 🥰")
         option = st.radio(' ', ('Iya', 'Engga'), horizontal=True)
-        cerita = st.text_area(label='Ceritanya gimana')
+        cerita = st.text_area(label='Cerita dikit disini')
         submit_button = st.form_submit_button(label='Kirim')
         if submit_button:
             col1.add({'nama' : name, "option": option, "tanggal": hari, "cerita": cerita})
@@ -78,7 +77,7 @@ def nikah():
 
     st.subheader('Cerita Terbaru')
 
-    doc = db.collection('pasangan')
+    doc = db.collection('nikah')
     datas = list(doc.stream())
     list_random = list(map(lambda x: x.to_dict(), datas))
     data = pd.DataFrame(list_random)
@@ -87,7 +86,7 @@ def nikah():
     for i,j,k,l in zip(data['nama'], data['cerita'], data['option'], data['tanggal']):
         cerita_list = list(j.split(" "))
         if k == 'Iya':
-            st.info(f'**{i.capitalize()}: {" ".join(cerita_list[:7])}  ({l.strftime("%Y-%m-%d")})**')
+            st.info(f'**{i.capitalize()}: {" ".join(cerita_list[:7])}**')
         else:
-            st.success(f'**{i.capitalize()} : {" ".join(cerita_list[:7])}  -- ({l.strftime("%Y-%m-%d")})**')
+            st.error(f'**{i.capitalize()} : {" ".join(cerita_list[:7])}**')
         st.write(j)

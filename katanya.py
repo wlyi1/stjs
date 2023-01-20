0de8ee52795ce2603cffc7c6d8ef47b514facab1
waliyudin sammadikun <waliyudin938@gmail.com>
2023-01-20 14:24:18 +0700

    add kerja section

--- katanya.py
+++ katanya.py
@@ -147,3 +147,45 @@ def ortu():
         else:
             st.error(f'**{i.capitalize()} : {" ".join(cerita_list[:7])}**')
         st.write(j)
+
+def kerja():
+    hari = dt.now() #.strftime('%Y-%m-%d %H:%M:%S')
+
+    st.header("Apakah kamu senang dengan pekerjaanmu? 👨‍🏭")
+    st.caption("Katanya punya aktivitas bekerja hari senin - jumat adalah idaman masyarakat karena kita merasa bermanfaat dan memperoleh uang tiap bulannya \
+        . Duit gajian bisa dipakai buat beli gadget atau jalan-jalan. Di lain sisi tidak jarang kita melihat berita yg pilu dimana ada pegawai yang bunuh diri karena bekerja \
+            . Jadi buat kamu yang sedang bekerja apakah hatimu senang?")
+
+    with st.form(key='form1', clear_on_submit=True):
+        
+        col1 = db.collection('kerja')
+        name = st.text_input('Nama/akronim/samaran/panggilan : ')
+        st.write("Apakah kamu senang dengan pekerjaanmu? 👨‍🏭")
+        option = st.radio(' ', ('Iya', 'Engga'), horizontal=True)
+        cerita = st.text_area(label='Cerita')
+        submit_button = st.form_submit_button(label='Kirim')
+        if submit_button:
+            col1.add({'nama' : name, "option": option, "tanggal": hari, "cerita": cerita})
+            st.write('Terimakasih 👍')
+
+
+
+    doc = db.collection('kerja')
+    datas = list(doc.stream())
+    list_random = list(map(lambda x: x.to_dict(), datas))
+    data = pd.DataFrame(list_random)
+    data = data.sort_values(by=['tanggal'], ascending=False)
+    df = data['option'].value_counts().rename_axis('option').reset_index(name='counts')
+    fig1, ax1 = plt.subplots(figsize=(3,3))
+    ax1.pie(df.counts, autopct='%1.1f%%', labels = df.option, colors = ['#2C74B3', '#EB5353'], startangle=90)
+    
+    st.subheader("**Persentase Polling**")
+    st.pyplot(fig1)
+    st.subheader('Ceritanya')
+    for i,j,k,l in zip(data['nama'], data['cerita'], data['option'], data['tanggal']):
+        cerita_list = list(j.split(" "))
+        if k == 'Iya':
+            st.info(f'**{i.capitalize()}: {" ".join(cerita_list[:7])}**')
+        else:
+            st.error(f'**{i.capitalize()} : {" ".join(cerita_list[:7])}**')
+        st.write(j)

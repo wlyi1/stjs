836ded3a933274f16c8dd684a931ca8c60c2d90a
waliyudin sammadikun <waliyudin938@gmail.com>
2023-01-20 17:33:26 +0700

    cek

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ import streamlit.components.v1 as components
 from streamlit.components.v1 import html
 from PIL import Image
 from PIL import ImageDraw
-import katanya #from katanya import *
+#import katanya
+from katanya import *
 
 image1 = 'https://raw.githubusercontent.com/wlyi1/stjs/main/banner.png'
 st.image(image1)
-#['pasangan', 'nikah', 'ortu', 'kerja']
-list_katanya = dir(katanya)
-opt = st.selectbox('Pilih katanya: ',list_katanya )
+
+#list_katanya = dir(katanya)
+opt = st.selectbox('Pilih katanya: ',['pasangan', 'nikah', 'ortu', 'kerja'] )
 match opt:
     case 'pasangan' : pasangan()
     case "nikah" : nikah()
